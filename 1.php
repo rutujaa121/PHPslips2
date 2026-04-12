c1f862f1cacea9dad913d070caa7d7d4366cdabb
rutujaa121 <rutujaaugale@gmail.com>
2026-04-12 06:59:04 -0700

    track numbers using session

---
+++ 1.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+if(isset($_SESSION["count"]))
+    {
+        $_SESSION["count"]++;
+    }
+    else
+        {
+            $_SESSION["count"]=1;
+        }
+  echo "position:".$_SESSION["count"];
+?>
