409ec2c0fc9b05c6761891058111f0337369f442
rutujaa121 <rutujaaugale@gmail.com>
2026-04-26 07:06:46 -0700

    add file 29.php

---
+++ 29.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Registration Form</title>
+
+    <script>
+        function showMessage() {
+            alert("Hello Good Morning");
+        }
+    </script>
+</head>
+
+<body onload="showMessage()">
+
+    <h2>Student Registration Form</h2>
+
+    <form>
+        <label>Student Name:</label><br>
+        <input type="text" name="name"><br><br>
+
+        <label>Roll Number:</label><br>
+        <input type="text" name="roll"><br><br>
+        <input type="submit" value="Register">
+    </form>
+
+</body>
+</html>
